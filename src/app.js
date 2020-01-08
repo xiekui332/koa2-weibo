adaa9794265c2d767960f249f8c31e7b0db8ecd0
xiekui <18334794858@163.com>
2020-01-08 17:37:55 +0800

    feat: 增加session 配置

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+
+const { REDIS_CONF } = require('./conf/db')
+
 const index = require('./routes/index')
 const users = require('./routes/users')
 
@@ -31,6 +36,25 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+
+
+
+// session
+app.keys = ['QWEabc_/*-']
+app.use(session({
+	key: 'project.sid',   // cookie name 默认是`koa.sid`
+	prefix: 'project:sess:',	// redis key 的前缀， 默认是 koa:sess:
+	cookie: {
+		path: '/',
+		httpOnly: true,
+		maxAge: 24 * 60 * 60 * 1000		// ms
+	},
+	// ttl: 24 * 60 * 60 * 1000,		// ms
+	store: redisStore({
+		all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+	})
+}))
+
 
 // routes
 app.use(index.routes(), index.allowedMethods())
